c332ade56dc211e36dd6100c2cd44073062661ba
SM_7337 <sm7337@users.noreply.github.com>
2026-01-10 06:38:37 +0530

    UI Redesign

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const canvas = document.querySelector("#unity-canvas");
+const loader = document.querySelector("#game-wrapper img");
 
 const predir = "autoreason-build/";
+
+let unityInstance = null;
 
 createUnityInstance(canvas, {
   dataUrl: predir + "Build/autoreason-build.data.unityweb",
@@ -10,6 +13,9 @@ createUnityInstance(canvas, {
   companyName: "Group 1",
   productName: "AutoReason",
   productVersion: "1.0",
+}).then((instance) => {
+  unityInstance = instance;
+  loader.style.display = "none";
 });
 
 const game = document.getElementById("game-wrapper");
@@ -17,4 +23,9 @@ const game = document.getElementById("game-wrapper");
 game.addEventListener("wheel", (e) => e.preventDefault(), { passive: false });
 game.addEventListener("mousedown", (e) => {
   if (e.button === 1) e.preventDefault();
+});
+
+document.querySelector("#fullscreen-btn").addEventListener("click", () => {
+  if (!unityInstance) return;
+  unityInstance.SetFullscreen(1);
 });
